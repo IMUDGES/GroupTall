48f23b879fe802ed6b377cbe7fd92fabeabc8ace
Dule <1326435210@qq.com>
2016-08-02 22:07:35 +0800

    08/02写完回宿舍

--- GroupTall/LoginViewController.swift
+++ GroupTall/LoginViewController.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+//extension UIView {
+//    @IBInspectable var cornerRadius:CGFloat {
+//        get {
+//            return self.layer.cornerRadius
+//        }
+//        set {
+//            self.layer.cornerRadius = newValue
+//            self.layer.masksToBounds = (newValue > 0)
+//        }
+//    }
+//}
+
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var tf_phoneNumber: UITextField!
---
+++ GroupTall/RoundButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundButton.swift
+//  GroupTall
+//
+//  Created by Dule on 8/2/16.
+//  Copyright © 2016 liuzhenyu. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+            self.layer.masksToBounds = (cornerRadius > 0)
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+}
