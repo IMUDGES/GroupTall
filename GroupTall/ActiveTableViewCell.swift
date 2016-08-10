e2b9a2b4cf7a187d67c8b4a374832dc70e8ce33e
Dule <1326435210@qq.com>
2016-08-10 19:51:23 +0800

    活动页面

---
+++ GroupTall/ActiveTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActiveTableViewCell.swift
+//  GroupTall
+//
+//  Created by Dule on 8/10/16.
+//  Copyright © 2016 liuzhenyu. All rights reserved.
+//
+
+import UIKit
+
+class ActiveTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
